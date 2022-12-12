2496a874197fcced328f437c04816b401aabdc19
Jesse Sullivan <jesse.denis.sullivan@gmail.com>
2022-12-12 02:40:28 -0500

    add confirmed changes for part two

--- lib/one.ml
+++ lib/one.ml
@@ -35,11 +35,10 @@ let exec (log : string) : int =
   let calorie_groups = mk_calorie_groups entries in
   let calorie_sums = List.map (List.fold_left (+) 0) calorie_groups in
   print_endline (show_calories calorie_sums);
-  let sorted_calorie_sums = List.sort compare calorie_sums in
-  0
-  (* let top_three_calorie_sum = 
-    sorted_calorie_sums.[0] +
-    sorted_calorie_sums.[1] +
-    sorted_calorie_sums.[2] in
-  top_three_calorie_sum *)
+  let sorted_calorie_sums = List.rev (List.sort Int.compare calorie_sums) in
+  let top_three_calorie_sum = 
+    (List.nth sorted_calorie_sums 0) +
+    (List.nth sorted_calorie_sums 1) +
+    (List.nth sorted_calorie_sums 2) in
+  top_three_calorie_sum
 end
